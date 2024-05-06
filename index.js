59e16c82da5bd7bff19b0c3f77ae8fb510d55440
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-06 13:47:29 -0400

    Passed 9 tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(action='go to the office'){
+    return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(flair='*'){
+    return function(adjective ='special'){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
